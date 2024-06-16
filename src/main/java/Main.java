abcb2660628684a674fb3d76cdd0e57039f17c1a
n.yarek <yaroslav.chexter@gmail.com>
2024-06-16 14:15:25 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.subtract(3, 9));
+        System.out.println(calc.add(45, 3));
+        System.out.println(calc.divide(25, 5));
+        System.out.println(calc.multiply(20, 16));
+    }
+}
